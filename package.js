93a47b568743a66eeb15ee237fb704e254f9579a
Jon James <jon@threering.com>
2014-04-02 16:29:00 -0400

    update package

--- package.js
+++ package.js
@@ -2,17 +2,10 @@ Package.describe({
   summary: "Reactive objects using EMCA5 Object features, without the need for getters and setters."
 });
 
-var path = Npm.require("path");
 Package.on_use(function(api) {
-
-  // Required packages
   api.use(["deps", "ejson", "underscore"], ["client", "server"]);
-
-  // Server and client side code
-  api.add_files([
-    path.join("lib","reactive-object.js")
-  ], ["client", "server"]);
-
+  api.export && api.export('ReactiveObject');
+  api.add_files("lib/reactive-object.js", ["client", "server"]);
 });
 
 Package.on_test(function(api) {
@@ -20,7 +13,5 @@ Package.on_test(function(api) {
   api.use(["tinytest", "reactive-object"], ["client", "server"]);
 
   // Server and client side tests
-  api.add_files([
-    path.join("lib","reactive-object-test.js")
-  ], ["client", "server"]);
+  api.add_files("lib/reactive-object-test.js", ["client", "server"]);
 });
